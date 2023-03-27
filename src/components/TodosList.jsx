fbc7476b3abbb9afbbe8ecbbfdb66fae6f37bc07
Muhammed Nafees Geelani <engnrnafees@gmail.com>
2023-03-27 14:07:37 +0300

    Add editing functionality for todo tasks

--- src/components/TodosList.jsx
+++ src/components/TodosList.jsx
@@ -1,11 +1,11 @@
 import TodoItem from '@/components/TodoItem';
-const TodosList = ({ todosProps, handleChange , delTodo}) => {
+const TodosList = ({ todosProps, handleChange, delTodo, setUpdate }) => {
     return (
       <ul>
         {todosProps.map((todo) => (
           <TodoItem key={todo.id} itemProp={todo}
            //setTodos={setTodos} 
-          handleChange={handleChange} delTodo={delTodo}/>
+          handleChange={handleChange} delTodo={delTodo} setUpdate={setUpdate}/>
         ))}
       </ul>
     );
